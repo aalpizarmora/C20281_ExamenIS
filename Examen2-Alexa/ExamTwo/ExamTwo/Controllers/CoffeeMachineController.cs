32fbe09e1aed6a2de13221c4f172cf1041c352dd
Alexa Alpizar Mora <c20281@ecci.ucr.ac.cr>
2026-07-06 20:46:51 -0600

    Se agrega el controller.

--- Examen2-Alexa/ExamTwo/ExamTwo/Controllers/CoffeeMachineController.cs
+++ Examen2-Alexa/ExamTwo/ExamTwo/Controllers/CoffeeMachineController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ExamTwo.Services.IActionResults;
 using ExamTwo.Dtos;
 
 namespace ExamTwo.Controllers
 {
     public class CoffeeMachineController : Controller
     {
-
         private readonly Database _db;
 
         public CoffeeMachineController(Database db)
@@ -35,52 +35,12 @@ namespace ExamTwo.Controllers
         [HttpPost("buyCoffee")]
         public ActionResult<string> BuyCoffee([FromBody] OrderRequest request)
         {
-            if (request.Order == null || request.Order.Count == 0)
-                return BadRequest("Ordem vacia.");
-
-            if (request.Payment.TotalAmount <= 0)
-                return BadRequest("Dinero insuficiente ");
-
             try
             {
                 var costoTotal = request.Order.Sum(o => _db.keyValues2.First(c => c.Key == o.Key).Value * o.Value);
 
-                if (request.Payment.TotalAmount < costoTotal)
-                { 
-                    return BadRequest("Dinero insuficiente ");
-                }
-
-
-                foreach (var cafe in request.Order)
-                {
-                    var selected = _db.keyValues.First(c => c.Key == cafe.Key).Key;
-                    if (cafe.Value > _db.keyValues[selected])
-                    {
-                        return $"No hay suficientes {selected} en la máquina.";
-                    }
-                    _db.keyValues[selected] -= cafe.Value;
-                }
-
                 var change = request.Payment.TotalAmount - costoTotal;
                 String result = $"Su vuelto es de: {change} colones. Desglose:";
-
-                foreach (var coin in _db.keyValues3.Keys.OrderByDescending(c => c))
-                {
-                    var count = Math.Min(change / coin, _db.keyValues3[coin]);
-                    if (count > 0)
-                    {
-                        result +=  $" {count} moneda de {coin},  ";              
-                        change -= coin * count;
-                    }
-                }
-
-
-                if (change > 0)
-                {
-                    return StatusCode(500, "No hay suficiente cambio en la máquina.");
-                }
-
-                return Ok(result);
             }
             catch (ArgumentException ex)
             {
